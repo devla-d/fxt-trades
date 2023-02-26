1be60d1bd4f170d4d30e2069c21b441e741c8a92
samuel aniekan <samuelaniekan680@gmail.com>
2023-02-26 18:09:34 +0100

    added static root

--- baseapp/settings.py
+++ baseapp/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = "assets/"
 
 STATICFILES_DIRS = ((os.path.join(BASE_DIR, "assets")),)
-
+STATIC_ROOT = "/home/fxttwmjy/public_html/assets/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

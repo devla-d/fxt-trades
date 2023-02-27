7dbb617246618c88338421f5d5eb2300fb0a043e
samuel aniekan <samuelaniekan680@gmail.com>
2023-02-27 03:05:57 +0100

    modified

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("index/", views.dash_board, name="dashboard"),
+    path("dashboard/", views.dash_board, name="dashboard"),
     path("withdraw-funds/", views.withdrawal_, name="withdrawal"),
     path("transactions/", views.transactions_, name="transactions_"),
     path("account-settings/", views.settings_, name="settings_"),
